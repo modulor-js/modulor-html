1a300ac4af887f9e80700827eb04f898ef383dd5
Zhukov <aleksandr.zhukov@sevenonemedia.de>
2018-11-22 16:53:57 +0100

    more tests

--- test/html/processing.test.js
+++ test/html/processing.test.js
@@ -41,6 +41,10 @@ describe('processing', () => {
       parsedString: prepareLiterals`foo ${value1} bar ${value2} baz`,
       expectedPreparedLiterals: 'foo {modulor_html_chunk:0} bar {modulor_html_chunk:1} baz',
       expectedReplacedTokens: 'foo 1 bar 2 baz',
+    },
+    {
+      parsedString: prepareLiterals`<${'div'}><${'span'}></${'span'}></${'div'}>`,
+      expectedPreparedLiterals: '<{modulor_html_chunk:0}><{modulor_html_chunk:1}></{modulor_html_chunk:2}></{modulor_html_chunk:3}>',
     }
   ];
 
@@ -183,57 +187,69 @@ describe('replaceDynamicTags', () => {
 
   const testSets = [
     {
-      input: '<{modulor_html_chunk:1}></{modulor_html_chunk:2}>',
-      expectation: '<modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:1}"></modulor-dynamic-tag>'
+      input: '<{modulor_html_chunk:0}></{modulor_html_chunk:1}>',
+      expectation: '<modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:0}"></modulor-dynamic-tag>'
     },
-    //{
-      //input: '<table attr-one="1"></table>',
-      //expectation: '<sanitize:table attr-one="1"></sanitize:table>'
-    //},
-    //{
-      //input: `<table attr-one="1"
-      //foo=bar bla baz="ok"
-      //></table>`,
-      //expectation: `<sanitize:table attr-one="1"
-      //foo=bar bla baz="ok"
-      //></sanitize:table>`
-    //},
-    //{
-      //input: `
-        //<table>
-          //<tr>
-            //<td>foo</td>
-            //<td></td>
-          //</tr>
-          //<tr>
-            //<td><div></div></td>
-          //</tr>
-        //</table>
-      //`,
-      //expectation: `
-        //<sanitize:table>
-          //<sanitize:tr>
-            //<sanitize:td>foo</sanitize:td>
-            //<sanitize:td></sanitize:td>
-          //</sanitize:tr>
-          //<sanitize:tr>
-            //<sanitize:td><div></div></sanitize:td>
-          //</sanitize:tr>
-        //</sanitize:table>
-      //`
-    //},
-    //{
-      //input: `
-        //<style>
-          //.foo > .bar {  }
-        //</style>
-      //`,
-      //expectation: `
-        //<sanitize:style>
-          //.foo > .bar {  }
-        //</sanitize:style>
-      //`
-    //},
+    {
+      input: '<{modulor_html_chunk:0}/>',
+      expectation: '<modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:0}"/>'
+    },
+    {
+      input: '<{modulor_html_chunk:0} foo="bar"/>',
+      expectation: '<modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:0}" foo="bar"/>'
+    },
+    {
+      input: `
+        <{modulor_html_chunk:0}>
+          <{modulor_html_chunk:1}>
+          </{modulor_html_chunk:2}>
+        </{modulor_html_chunk:3}>
+      `,
+      expectation: `
+        <modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:0}">
+          <modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:1}">
+          </modulor-dynamic-tag>
+        </modulor-dynamic-tag>
+      `
+    },
+    {
+      input: `
+        <{modulor_html_chunk:0} foo="{modulor_html_chunk:1}" {modulor_html_chunk:1}="{modulor_html_chunk:2}">
+          <{modulor_html_chunk:3}
+            bla="test">
+          </{modulor_html_chunk:4}>
+        </{modulor_html_chunk:5}>
+      `,
+      expectation: `
+        <modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:0}" foo="{modulor_html_chunk:1}" {modulor_html_chunk:1}="{modulor_html_chunk:2}">
+          <modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:3}"
+            bla="test">
+          </modulor-dynamic-tag>
+        </modulor-dynamic-tag>
+      `
+    },
+    {
+      input: `
+        <{modulor_html_chunk:0}>
+          {modulor_html_chunk:1}
+          <{modulor_html_chunk:2}>
+            {modulor_html_chunk:3}
+          </{modulor_html_chunk:4}>
+          {modulor_html_chunk:5}
+        </{modulor_html_chunk:6}>
+        {modulor_html_chunk:7}
+      `,
+      expectation: `
+        <modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:0}">
+          {modulor_html_chunk:1}
+          <modulor-dynamic-tag modulor-chunk="{modulor_html_chunk:2}">
+            {modulor_html_chunk:3}
+          </modulor-dynamic-tag>
+          {modulor_html_chunk:5}
+        </modulor-dynamic-tag>
+        {modulor_html_chunk:7}
+      `
+    },
   ]
   testSets.forEach((testSet, index) => {
     it(`set #${index}`, () => {
